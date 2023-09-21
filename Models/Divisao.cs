f96680cc332260ae371323453a3dc1b8f2de6243
Elsondagama <Elsondagama@gmail.com>
2023-09-21 23:23:50 +0100

    Primeiro Commit

---
+++ Models/Divisao.cs
@@ -0,0 +1,15 @@
+﻿namespace CleanZone.Models
+{
+    public class Divisao
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int CleanTime { get; set; }
+        public int CleanInterval { get; set;}
+
+
+    }
+
+
+
+}
